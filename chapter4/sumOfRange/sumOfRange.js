5fce62717d95c17908215f042c27c45b8b51c1a5
NarothamSai <sai.narotham999@gmail.com>
2021-01-30 18:52:03 +0530

    Update styling for sumOfRange

--- chapter4/sumOfRange/sumOfRange.js
+++ chapter4/sumOfRange/sumOfRange.js
@@ -1,18 +1,18 @@
-function sum(arr_num) {
-  let sum_num = 0;
-  if (Array.isArray(arr_num)) {
-    arr_num.forEach((elem) => (sum_num += elem));
+function sum(arrNum) {
+  let sumNum = 0;
+  if (Array.isArray(arrNum)) {
+    arrNum.forEach((elem) => (sumNum += elem));
   }
-  return sum_num;
+  return sumNum;
 }
 
 function range(start, end) {
-  let range_num = [];
+  let rangeNum = [];
   for (let ctr = start; ctr <= end; ctr += 1) {
-    range_num.push(ctr);
+    rangeNum.push(ctr);
   }
-  console.log(range_num);
-  return range_num;
+  console.log(rangeNum);
+  return rangeNum;
 }
 
 module.exports = { sum, range };
