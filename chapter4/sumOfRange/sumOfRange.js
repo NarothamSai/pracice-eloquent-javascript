f313d2dc7c62a123e2da0db26c47ccd25d0c4ac2
NarothamSai <sai.narotham999@gmail.com>
2021-01-30 18:44:56 +0530

    Add sumOfRange sum and range functions

--- chapter4/sumOfRange/sumOfRange.js
+++ chapter4/sumOfRange/sumOfRange.js
@@ -1,5 +1,18 @@
-function sum() {}
+function sum(arr_num) {
+  let sum_num = 0;
+  if (Array.isArray(arr_num)) {
+    arr_num.forEach((elem) => (sum_num += elem));
+  }
+  return sum_num;
+}
 
-function range() {}
+function range(start, end) {
+  let range_num = [];
+  for (let ctr = start; ctr <= end; ctr += 1) {
+    range_num.push(ctr);
+  }
+  console.log(range_num);
+  return range_num;
+}
 
 module.exports = { sum, range };
